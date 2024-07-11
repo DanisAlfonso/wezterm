8e925f9baa708ff0a6e702ea119491bccf96beaa
Danis <danis.ramirez.hn@gmail.com>
2024-07-11 22:45:53 +0200

    change color

--- wezterm.lua
+++ wezterm.lua
@@ -3,16 +3,16 @@ local wezterm = require("wezterm")
 -- Function to determine color scheme based on system appearance
 local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
-		return "Colorful Colors (terminal.sexy)" -- Replace with your preferred dark mode scheme
+		return "Catppuccin Mocha" -- Replace with your preferred dark mode scheme
 	else
-		return "Catppuccin Latte (Gogh)" -- Replace with your preferred light mode scheme
+		return "Catppuccin Latte" -- Replace with your preferred light mode scheme
 	end
 end
 
 local config = {
 	font_size = 14.0, -- Set your desired font size here
-	window_background_opacity = 0.90, -- Set your desired opacity level here
-	initial_cols = 120, -- Set your desired number of columns here
+	window_background_opacity = 1.0, -- Set your desired opacity level here
+	initial_cols = 130, -- Set your desired number of columns here
 	initial_rows = 39, -- Set your desired number of rows here
 
 	-- Set the initial color scheme based on the current appearance
